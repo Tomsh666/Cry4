b51c89d0a109a7f1a9cc965512075b8bbafa9eae
Tomsh666 <tomshinskiy02@gmail.com>
2023-06-03 01:18:44 +0300

    table rdy mul need to fix after needed all groups

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import group_elems
+import itertools
+
+def get_group_elements(n):
+    elements = []
+
+    # Генерация всех возможных перестановок длиной n
+    permutations = list(itertools.permutations(range(1, n + 1)))
+
+    # Преобразование перестановок в строковое представление
+    for perm in permutations:
+        perm_str = "".join(str(p) for p in perm)
+        #elements.append(int(perm_str))
+        elements.append(perm_str)
+    return elements
+
 def mul_table(elements):
     # Создаем пустой DataFrame для таблицы умножения
     table = pd.DataFrame(index=elements, columns=elements)
@@ -8,18 +23,20 @@ def mul_table(elements):
     for element1 in elements:
         for element2 in elements:
             # Выполняем умножение элементов element1 и element2
-            result = ''.join(sorted(set(element1) & set(element2), key=element1.index))
+            result = eval(element1+element2)
             # Обновляем соответствующую ячейку в таблице
             table.loc[element1, element2] = result
     # Выводим таблицу умножения
     print(table)
 
 
-
+#ввод
 #print(n=)
 #n=int(input())
 n=3
-elems=group_elems.get_group_elements(n)
+
+#начало проги
+elems=get_group_elements(n)
 print('Элементы S_{l}'.format(l=n),elems)
 print('Таблица умножения элементов S_{l}:'.format(l=n))
 mul_table(elems)
